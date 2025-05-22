2d219b28c67ff82a03ab62d87a2d9483bd34e060
Cyruzzzz <montenge90@gmail.com>
2025-05-22 08:58:06 -0300

    sadsad

--- Python/Ejecicios/Ejercicios_listas.py
+++ Python/Ejecicios/Ejercicios_listas.py
@@ -1,22 +1,18 @@
 #Ejercicio 1: Gestión de una lista de compras
 lista_compras = []
-
 lista_compras.append("leche")
 lista_compras.append("pan")
 lista_compras.append("huevos")
 
 
 print("La lista de compras tiene", (len(lista_compras)), "productos.")
-
 lista_compras.pop(2)
-
 print(lista_compras)
 
 
 #Ejercicio 2: Contar apariciones
 colores = ["rojo", "azul", "verde", "rojo", "azul", "rojo"]
 print("El color rojo aparece", (colores.count("rojo")), "veces en la lista.")
-
 colores[2]='amarillo'
 print(colores)
 
@@ -32,8 +28,6 @@ print("Nombre:", estudiante["nombre"])
 print("Edad:", estudiante["edad"])
 
 estudiante["materias"].append("Inglés")
-
-
 print("El estudiante esta cursando ", len(estudiante["materias"]), "materias.")
 print("Materias:", estudiante["materias"])
 print("El estudiante tiene un promerio de ", (estudiante.get("promedio", 0)),".")
